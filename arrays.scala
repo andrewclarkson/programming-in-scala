6df4372351005402b50208930375675ad9cdf33b
Andrew Clarkson <andrew@clarkson.mn>
2014-10-16 12:44:47 -0500

    equivalent operations

--- arrays.scala
+++ arrays.scala
@@ -1,8 +1,8 @@
 val greetings = new Array[String](3)
 
-greetings(0) = "Hi"
-greetings(1) = "Hello"
-greetings(2) = "What's up?"
+greetings.update(0, "Hi")
+greetings.update(1, "Hello")
+greetings.update(2, "What's up?")
 
 greetings.foreach(println)
 

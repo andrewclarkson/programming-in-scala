3ab4291d98396e21e8b6c166969bd1a7a15e1dfc
Andrew Clarkson <andrew@clarkson.mn>
2014-10-16 12:45:54 -0500

    More concise

--- arrays.scala
+++ arrays.scala
@@ -1,8 +1,4 @@
-val greetings = new Array[String](3)
-
-greetings.update(0, "Hi")
-greetings.update(1, "Hello")
-greetings.update(2, "What's up?")
+val greetings = Array("Hi", "Hello", "What's up?")
 
 greetings.foreach(println)
 

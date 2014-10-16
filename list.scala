36bad5a31ec6ab6ed4c4ae820700925f0ea0d5d4
Andrew Clarkson <andrewclarkson@live.com>
2014-10-16 13:18:41 -0500

    Pipelining operations on lists

--- list.scala
+++ list.scala
@@ -2,5 +2,7 @@ val greetings = "Hi" :: "Hello" :: "What's up?" :: Nil
 
 val goodbyes = List("Goodbye", "Farewell", "See you later")
 
-(greetings ::: goodbyes).foreach(println)
+val combined = greetings ::: goodbyes
+
+combined.drop(2).dropRight(2).foreach(println)
 

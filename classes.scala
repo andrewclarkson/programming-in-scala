7addecc943849c89a1f936e19d3550849bd6575d
Andrew Clarkson <andrewclarkson@live.com>
2014-10-17 12:17:45 -0500

    Super concise functional style

--- classes.scala
+++ classes.scala
@@ -2,8 +2,6 @@ import scala.collection.mutable.Map
 
 class ChecksumAccumulator {
   private var sum = 0
-  
-  def add(byte: Byte) { sum += byte }
   def checksum(): Int = ~(sum & 0xFF) + 1
 }
 
@@ -11,17 +9,17 @@ object ChecksumAccumulator {
   private val cache = Map[String, Int]()
 
   def calculate(string: String): Int = 
-    if(cache.contains(string))
-      cache(string)
-    else {
-      val accumulator = new ChecksumAccumulator
-      for(byte <- string.getBytes()) {
-        accumulator.add(byte)
+    cache get string match {
+      case Some(checksum) => checksum
+      case None => {
+        val accumulator = new ChecksumAccumulator
+        accumulator.sum = string.getBytes.sum
+        val checksum = accumulator.checksum()
+        cache put (string, checksum)
+        checksum
       }
-      val checksum = accumulator.checksum()
-      cache += (string -> checksum)
-      checksum
     }
+
 }
 
 println(ChecksumAccumulator.calculate("Hello World"))

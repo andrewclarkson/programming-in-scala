2af6f006e22a02daed23282e6ba5b43ea3587d4c
Andrew Clarkson <andrewclarkson@live.com>
2014-10-19 16:04:08 -0500

    Create a precondition

--- objects.scala
+++ objects.scala
@@ -1,6 +1,7 @@
 class Rational(numerator: Int, denominator: Int) {
+  require(denominator != 0)
   override def toString = numerator + "/" + denominator
 }
 
-val three_fourths = new Rational(3, 4);
+val three_fourths = new Rational(3, 4)
 println(three_fourths)

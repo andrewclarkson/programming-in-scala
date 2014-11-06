f7a8e3c17509c7034193ba91abb734f24faf20be
Andrew Clarkson <andrew@clarkson.mn>
2014-11-06 16:20:25 -0600

    fun with unicode and character literals

--- literals.scala
+++ literals.scala
@@ -12,3 +12,23 @@ val five_double = 5.0
 val five_million = 5e6
 
 println(five_long, five_float, five_double, five_million)
+
+val a = 'a'
+
+// deprecated
+// val a_ascii = '\141'
+
+val a_unicode = '\u0061'
+
+val he\u0061rt = '\u2764'
+
+val backslash = '\\'
+
+
+/*
+ * If the heart doesn't show check 
+ * > System.getProperty("file.encoding");
+ * this can be set by:
+ * $ export JAVA_OPTS="-Dfile.encoding=UTF-8"
+ */
+println(a, a_unicode, heart, backslash)

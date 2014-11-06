81de54cb30a422872dda3ee543c8955e2a56eccf
Andrew Clarkson <andrew@clarkson.mn>
2014-11-06 15:56:54 -0600

    Use integer parsing for other bases

--- literals.scala
+++ literals.scala
@@ -1,8 +1,7 @@
 val five = 5
 val hex_five = 0x5
 
-// Invalid
-//val octal_five = 05
-//val binary_five = b101
+val octal_five = Integer.parseInt("05", 8);
+val binary_five = Integer.parseInt("101", 2);
 
 println(five, hex_five, octal_five, binary_five)

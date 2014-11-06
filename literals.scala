55ec3189e1b22f12636b04e3252e4469dbeb4752
Andrew Clarkson <andrew@clarkson.mn>
2014-11-06 16:01:25 -0600

    other types of numeric literals

--- literals.scala
+++ literals.scala
@@ -5,3 +5,10 @@ val octal_five = Integer.parseInt("05", 8);
 val binary_five = Integer.parseInt("101", 2);
 
 println(five, hex_five, octal_five, binary_five)
+
+val five_long = 5L
+val five_float = 5f
+val five_double = 5.0
+val five_million = 5e6
+
+println(five_long, five_float, five_double, five_million)

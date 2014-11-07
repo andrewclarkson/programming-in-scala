6d0d5ea84bb85fe9c027aa870a9199bfe6fb0a5e
Andrew Clarkson <andrew@clarkson.mn>
2014-11-06 18:55:31 -0600

    One of the weirder things you could do

--- symbols.scala
+++ symbols.scala
@@ -7,3 +7,15 @@ def converter(t: Symbol, data: String): Any = t match {
 println(converter('string, "hello"))
 println(converter('int, "5"))
 println(converter('char, "☭"))
+
+case class Bank(balance: Int) {
+  def apply(method: Symbol)(amount: Int): Bank = method match {
+    case 'deposit => Bank(balance + amount)
+    case 'withdraw => Bank(balance - amount)
+    case 'print => {
+      println(balance)
+      Bank(balance)
+    }
+  }
+}
+Bank(10)('deposit)(10)('withdraw)(10)('print)(8888)
